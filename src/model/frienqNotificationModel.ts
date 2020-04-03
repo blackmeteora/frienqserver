8a8ebf74350841ef875c9d1dc680a85dbc1c334d
Cafer Aydın <cafer_aydin@hotmail.com.tr>
2020-04-04 02:04:16 +0300

    Release Commit

--- src/model/frienqNotificationModel.ts
+++ src/model/frienqNotificationModel.ts
@@ -80,7 +80,7 @@ export default class FrienqNotificationModel {
                         data2 : item.notification_data2,
                         uid_member : frienq.uid,
                         status : "done",
-                        icon : `http://5.11.131.101:3000/Frienq/GetProfilePicture/?u=${frienq.uid}&f=${frienq.profile_picture}.jpg`
+                        icon : `/Frienq/GetProfilePicture/?u=${frienq.uid}&f=${frienq.profile_picture}.jpg`
                     },
                     condition : `'${uid}' in topics`
             }
